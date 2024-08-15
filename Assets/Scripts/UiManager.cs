e041eae7f06418419f6744f228d4acfcc58d181d
tranminhtuan68003 <Tranminhtuan06082003@gmail.com>
2024-08-15 09:55:29 +0700

    Update

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+}
